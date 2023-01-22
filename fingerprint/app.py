0dd2335af9933edca8ad0633d37cb899c6d74fd5
bevennyamande <bevennyamande@gmail.com>
2023-01-22 19:45:07 +0200

    test

--- fingerprint/app.py
+++ fingerprint/app.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template, url_for, request
 import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = os.urandom(34)
 
-@app.route('/')
-@app.route('/index.html', methods=['GET', 'POST'])
+UPLOAD_FOLDER = 'temp'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/',methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
+@app.route('/process', methods=['POST'])
+def process():
+    # find the file in the request parameter and process to the folder containing the fingerprints
+    # also engage the algorithm here
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
